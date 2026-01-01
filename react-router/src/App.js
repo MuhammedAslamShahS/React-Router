971068c61d75714a10847d1ce0f1d1c2b431a67b
Muhammed Aslam Shah S <muhammedaslamshahsofficial@gmail.com>
2026-01-01 10:31:45 -0800

    2nd commit : {Impletement React Router Dom , make Links on Header and Successfully complete that work}

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Project from "./components/Project/Project";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" exact Component={Project} />
+                <Route path="/about" Component={About} />
+                <Route path="/" Component={Project} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
---
+++ react-router/src/components/Project/Project.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Project = () => {
+  return (
+    <div>
+      <h1>React Router</h1>
+      <h1>Project</h1>
+      <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolores, molestias id mollitia ut, neque dolore enim illo assumenda tenetur laudantium voluptas officiis harum pariatur? Nisi saepe, facilis molestias mollitia nostrum adipisci ullam dolore, eos officia quae incidunt aspernatur velit fugit! Porro dolorem ea nihil voluptatibus hic quis doloremque nobis pariatur?</p>
+    </div>
+  )
+}
+
+export default Project
